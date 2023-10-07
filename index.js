33915d514e82d4db27a63739fe92fca40d39b086
Rizvan <rizvankurungattil@gmail.com>
2023-10-07 15:55:23 +0530

    initial testing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+const { connectDB } = require("./config/db.config");
+require("dotenv").config();
+// const PORT = process.env.PORT || 3000;
+// const saltRounds = 5;
+// const myPlaintextPassword = "s0//P4$$w0rD";
+// const someOtherPlaintextPassword = "not_bacon";
+const app = express();
+app.use(express.json());
+
+app.use("/user", userRoutes);
+
+connectDB().then(() => {
+  app.listen(() => {
+    console.log(`server is running`);
+  });
+});
