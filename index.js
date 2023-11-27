8786981bc370bbc78c99c336c4b39929dad1f5e1
Rizvan <rizvankurungattil@gmail.com>
2023-11-27 12:30:55 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {dynamoClient,TABLE_NAME}=require('./config/dynamo')
 const {ulrRoutes}=require('./routes/urlRoutes')
-
 require("dotenv").config();
+const cors=require('cors')
 const PORT = process.env.PORT || 3000;
 const app=express()
+app.use(cors())
 app.use(express.json())
 app.use('/url',ulrRoutes)
 
